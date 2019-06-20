1b490b786b714bb9a4fe7a0637ff3fff674f1549
davidorjuela <davidorjuel@gmail.com>
2019-06-19 19:27:52 -0500

    sinatra modificar amigo

--- app.rb
+++ app.rb
@@ -16,8 +16,28 @@ end
 
 post '/friends' do
 #  if(params[:name].length>0 && params[:last_name].length>0 && params[:age].to_i>0 && params[:age].to_i.is_a? Integer)
-    friends << params
+  #if params[:friend[:name]]=~/[A-Z]{2,20}/ix && params[:friend[:last_name]]=~/[A-Z]{2,20}/ix && params[:friend[:age]]=~/\d/ && params[:friend[:age]].to_i>0 && params[:friend[:age]].to_i<120
+  #validar << params[:friend]
+  #if params[:friend[:name]].length>0
+    friends << params[:friend]
     @friends=friends
-#  end
+  #end
   redirect '/'
+end
+
+get '/friends/new_friend' do
+  erb :new_friend
+end
+
+get '/friends/:id' do
+  index=params[:id].to_i
+  @friend=friends[index]
+  erb :show
+end
+
+get '/friends/:id/edit' do
+  index=params[:id].to_i
+  @friend=friends[index]
+  @index=index
+  erb :edit
 end
